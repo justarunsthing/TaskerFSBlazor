693a636e2427fd710deb10ff50c6bdaf13a29394
justarunsthing <justarunsthing@outlook.com>
2026-01-12 07:55:29 +0000

    Added TaskerItem class

---
+++ TaskerFSBlazor.Client/Models/TaskerItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskerFSBlazor.Client.Models
+{
+    public class TaskerItem
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Every task must have a name")]
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; } = false;
+    }
+}
