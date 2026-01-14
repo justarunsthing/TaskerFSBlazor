caf8dc6b09d58fbb4fb124b5c7af2dd924cc96d9
justarunsthing <justarunsthing@outlook.com>
2026-01-14 08:22:34 +0000

    Added migration for DbTaskerItem class

--- TaskerFSBlazor/Migrations/20260114082058_004-add-DbTaskerItem-class.Designer.cs
+++ TaskerFSBlazor/Migrations/20260114082058_004-add-DbTaskerItem-class.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TaskerFSBlazor.Data;
@@ -11,9 +12,11 @@ using TaskerFSBlazor.Data;
 namespace TaskerFSBlazor.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260114082058_004-add-DbTaskerItem-class")]
+    partial class _004addDbTaskerItemclass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
