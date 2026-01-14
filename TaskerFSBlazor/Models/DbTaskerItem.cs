a3f9cd925b93f8462f56a7601abca0d0089ffe68
justarunsthing <justarunsthing@outlook.com>
2026-01-14 08:17:44 +0000

    Added DbTaskerItem model

---
+++ TaskerFSBlazor/Models/DbTaskerItem.cs
@@ -0,0 +1,13 @@
+﻿using TaskerFSBlazor.Data;
+using TaskerFSBlazor.Client.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskerFSBlazor.Models
+{
+    public class DbTaskerItem : TaskerItem
+    {
+        [Required]
+        public string? UserId { get; set; }
+        public virtual ApplicationUser? User { get; set; }
+    }
+}
