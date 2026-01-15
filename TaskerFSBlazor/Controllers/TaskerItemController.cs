85280902b7c15734597f0b087c5ec8f292f1b2a8
justarunsthing <justarunsthing@outlook.com>
2026-01-15 08:04:44 +0000

    Added TaskerItemController

---
+++ TaskerFSBlazor/Controllers/TaskerItemController.cs
@@ -0,0 +1,22 @@
+﻿using TaskerFSBlazor.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TaskerFSBlazor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TaskerItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TaskerItemController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+    }
+}
