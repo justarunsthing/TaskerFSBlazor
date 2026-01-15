627bdcaaee57038041dd27491445c56d850ffcc6
justarunsthing <justarunsthing@outlook.com>
2026-01-15 08:22:05 +0000

    Register HttpClient

--- TaskerFSBlazor.Client/Program.cs
+++ TaskerFSBlazor.Client/Program.cs
@@ -5,5 +5,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
